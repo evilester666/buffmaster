5a938e1fced4652d3cad0dfc7b16dc5dd3cc2e13
evilester <1627758661@qq.com>
2024-06-04 11:36:58 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // import { useState } from 'react'
-import React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import React from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 
 import Game from './components/Game';
 import './App.css'
 
-import WebApp from '@twa-dev/sdk'
+// import WebApp from '@twa-dev/sdk'
 
 function App() {
 
